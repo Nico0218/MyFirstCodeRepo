ddf5619405b519ca3c251d76c86259d6b49fa629
Nicolaas Coetzee <nicolaas.willem.coetzee7@gmail.com>
2022-07-20 18:55:23 +0200

    My first code commit of my first demo app

---
+++ MyFirstApp1/MyFirstApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyFirstApp1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("i nuwe lyn");
+
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Some random value");
+
+            Console.WriteLine();
+        }
+    }
+
+    internal class TestClass { 
+    }
+}
